c4f225e09f9d344ec6e08172cfd7500ac45d047e
Nirupan Joe <nirupanjoej@gmail.com>
2024-08-12 17:10:12 +0530

    Created the addProduct.

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -2,10 +2,35 @@ const Product = require('../models/product');
 const Status = require('../utils/statusEnum');
 
 exports.getProducts = async (req, res) => {
-	const products = await Product.find();
+	try {
+		const products = await Product.find({});
 
-	res.status(Status.OK.code).json({
-		status: Status.OK.message,
-		products: products,
-	});
+		res.status(Status.OK.code).json({
+			status: Status.OK.message,
+			products: products,
+		});
+	}
+	catch (err) {
+		res.status(Status.BAD_REQUEST.code).json({
+			status: Status.BAD_REQUEST.message,
+			error: err,
+		});
+	}
+};
+
+exports.addProduct = async (req, res) => {
+	try {
+		const product = await Product.create(req.body);
+
+		res.status(Status.CREATED.code).json({
+			status: Status.CREATED.message,
+			product: product,
+		});
+	}
+	catch (err) {
+		res.status(Status.BAD_REQUEST.code).json({
+			status: Status.BAD_REQUEST.message,
+			error: err,
+		});
+	}
 };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getProducts } = require('../controllers/product');
+const { getProducts, addProduct } = require('../controllers/product');
 const router = express.Router();
 
-router.route('/products').get(getProducts);
+router.route('/products').get(getProducts)
+	.post(addProduct);
 
 module.exports = router;
