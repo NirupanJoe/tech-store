67a9f98fcdbd67edb592e43abe98dc257c5fa832
Nirupan Joe <nirupanjoej@gmail.com>
2024-08-12 18:09:27 +0530

    Created the getProduct.

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -34,3 +34,28 @@ exports.addProduct = async (req, res) => {
 		});
 	}
 };
+
+// eslint-disable-next-line max-lines-per-function
+exports.getProduct = async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+
+		if(!product) {
+			return res.status(Status.NOT_FOUND.code).json({
+				status: Status.NOT_FOUND.message,
+				message: 'Product not found',
+			});
+		}
+
+		res.status(Status.OK.code).json({
+			status: Status.OK.message,
+			product: product,
+		});
+	}
+	catch (err) {
+		res.status(Status.BAD_REQUEST.code).json({
+			status: Status.BAD_REQUEST.message,
+			error: err.message,
+		});
+	}
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { getProducts, addProduct } = require('../controllers/product');
+const {
+	getProducts,
+	addProduct,
+	getProduct,
+} = require('../controllers/product');
+
 const router = express.Router();
 
 router.route('/products').get(getProducts)
 	.post(addProduct);
+
+router.route('/products/:id').get(getProduct);
 
 module.exports = router;
