911c4bcd786620f5e7b98228e1132cc6e3c90cc9
Nirupan Joe <nirupanjoej@gmail.com>
2024-08-13 21:46:17 +0530

    Different error for development and production.

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,12 +1,27 @@
 const Status = require('../utils/statusEnum');
 
+const handleDevelopmentError = (err) => ({
+	error: err,
+	stack: err.stack,
+});
+
+const handleProductionError = () => ({});
+
+const errorHandlers = {
+	development: handleDevelopmentError,
+	production: handleProductionError,
+};
+
 module.exports = (
 	err, req, res, _next,
 ) => {
+	const env = process.env.NODE_ENV;
+
+	const handleError = errorHandlers[env] || handleProductionError;
+
 	res.status(err.statusCode || Status.INTERNAL_SERVER_ERROR.code).json({
 		success: false,
 		message: err.message || Status.INTERNAL_SERVER_ERROR.message,
-		error: err,
-		stack: err.stack,
+		...handleError(err),
 	});
 };
