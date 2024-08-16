859560eb2053ce2dfdde466d4fa281a30bd7e9fb
Nirupan Joe <nirupanjoej@gmail.com>
2024-08-16 23:33:50 +0530

    Added the cookie-parser.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const errorMiddleware = require('./middleware/error');
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 const products = require('./routes/product');
 const auth = require('./routes/auth');
