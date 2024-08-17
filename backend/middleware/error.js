28d7279813f63b6b36db23c6764d1ed95eacc0f9
Nirupan Joe <nirupanjoej@gmail.com>
2024-08-17 16:56:37 +0530

    Created the errorNameHandler and errorCodeHandler.

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,6 +1,35 @@
 const ErrorHandler = require('../utils/ErrorHandler');
 const Status = require('../utils/statusEnum');
 
+const errorNamesHandlers = {
+	ValidationError: (err) => {
+		const message = Object.values(err.errors)
+			.map(({ message: errorMessage }) => errorMessage);
+
+		return new ErrorHandler(message, Status.BAD_REQUEST.code);
+	},
+
+	JSONWebTokenError: () => {
+		const message = 'JSON Web Token is invalid. Try again';
+
+		return new ErrorHandler(message, Status.UNAUTHORIZED.code);
+	},
+
+	TokenExpiredError: () => {
+		const message = 'JSON Web Token is expired. Try again';
+
+		return new ErrorHandler(message, Status.UNAUTHORIZED.code);
+	},
+};
+
+const errorCodesHandlers = {
+	11000: (err) => {
+		const message = `Duplicate field ${ Object.keys(err.keyValue) } value entered`;
+
+		return new ErrorHandler(message, Status.BAD_REQUEST.code);
+	},
+};
+
 const handleDevelopmentError = (err) => ({
 	error: err,
 	stack: err.stack,
@@ -9,12 +38,12 @@ const handleDevelopmentError = (err) => ({
 const handleProductionError = (err) => {
 	let error = { ...err };
 
-	if(err.name === 'ValidationError') {
-		const message = Object.values(err.errors)
-			.map(({ message: errorMessage }) => errorMessage);
+	if(errorNamesHandlers[err.name])
+		error = errorNamesHandlers[err.name](err);
 
-		error = new ErrorHandler(message, Status.BAD_REQUEST.code);
-	}
+	if(errorCodesHandlers[error.code])
+		error = errorCodesHandlers[err.code](err);
+
 	return { message: error.message || Status.INTERNAL_SERVER_ERROR.message };
 };
 
