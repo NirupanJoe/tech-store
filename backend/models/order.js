f3b0845d939d0c64206c19accaf7a79ea7a49ab1
Nirupan Joe <nirupanjoej@gmail.com>
2024-08-18 12:37:17 +0530

    Created the orderSchema.

---
+++ backend/models/order.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const orderSchema = new mongoose.Schema({
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+	orderItems: [
+		{
+			product: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Product', required: true,
+			},
+			name: { type: String, required: true },
+			qty: { type: Number, required: true },
+			price: { type: Number, required: true },
+			image: { type: String, required: true },
+		},
+	],
+	shippingAddress: {
+		address: { type: String, required: true },
+		city: { type: String, required: true },
+		postalCode: { type: String, required: true },
+		country: { type: String, required: true },
+		phoneNo: {
+			type: String,
+			required: true,
+			validate: [
+				validator.isMobilePhone,
+				'Please Enter A Valid Phone Number',
+			],
+		},
+	},
+	paymentMethod: { type: String, required: true },
+	paymentResult: {
+		id: { type: String },
+		status: { type: String },
+		updateTime: { type: String },
+		emailAddress: { type: String },
+	},
+	itemsPrice: { type: Number, required: true, default: 0.0 },
+	taxPrice: { type: Number, required: true, default: 0.0 },
+	shippingPrice: { type: Number, required: true, default: 0.0 },
+	totalPrice: { type: Number, required: true, default: 0.0 },
+	isPaid: { type: Boolean, required: true, default: false },
+	paidAt: { type: Date },
+	isDelivered: { type: Boolean, required: true, default: false },
+	deliveredAt: { type: Date },
+}, {
+	timestamps: true,
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
