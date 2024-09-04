1426336b2577806e67157fedc187d2cd20b0fc7a
Nirupan Joe <nirupanjoej@gmail.com>
2024-09-04 21:15:43 +0530

    Displayed the products by api data.

--- frontend/src/components/Pages/Home/Products.jsx
+++ frontend/src/components/Pages/Home/Products.jsx
@@ -1,94 +1,11 @@
 import { Fragment } from 'react';
 
-const products = [
-	{
-		title: 'Nebula Fold Pro',
-		color: 'Silver Shadow',
-		storage: '256 GB',
-		image: 'https://via.placeholder.com/150x200',
-		price: '₹152519.00',
-		discountedPrice: '₹164999.00',
-		discount: '₹12480.00',
-		monthlyPayment: '₹7395.13',
-	},
-	{
-		title: 'Nebula Flip Pro',
-		color: 'Blue',
-		storage: '256 GB',
-		image: 'https://via.placeholder.com/150x200',
-		price: '₹101369.00',
-		discountedPrice: '₹109999.00',
-		discount: '₹8630.00',
-		monthlyPayment: '₹4915.04',
-	},
-	{
-		title: 'Nebula Tab Ultra (Wi-Fi, 8GB RAM)',
-		color: 'Lavender',
-		storage: '256 GB',
-		image: 'https://via.placeholder.com/150x200',
-		price: '₹43199.00',
-		discountedPrice: '₹51999.00',
-		discount: '₹8800.00',
-		monthlyPayment: '₹2094.57',
-	},
-	{
-		title: 'Nebula Tab Pro (Wi-Fi, 12GB RAM)',
-		color: 'Graphite',
-		storage: '512 GB',
-		image: 'https://via.placeholder.com/150x200',
-		price: '₹84999.00',
-		discountedPrice: '₹94999.00',
-		discount: '₹10000.00',
-		monthlyPayment: '₹4250.00',
-	},
-	{
-		title: 'Nebula Fold Lite',
-		color: 'Green',
-		storage: '128 GB',
-		image: 'https://via.placeholder.com/150x200',
-		price: '₹102499.00',
-		discountedPrice: '₹119999.00',
-		discount: '₹17500.00',
-		monthlyPayment: '₹5124.00',
-	},
-	{
-		title: 'Nebula Fold Lite',
-		color: 'Blue',
-		storage: '256 GB',
-		image: 'https://via.placeholder.com/150x200',
-		price: '₹102499.00',
-		discountedPrice: '₹119999.00',
-		discount: '₹17500.00',
-		monthlyPayment: '₹5124.00',
-	},
-	{
-		title: 'Nebula Flip Lite',
-		color: 'Pink',
-		storage: '128 GB',
-		image: 'https://via.placeholder.com/150x200',
-		price: '₹74999.00',
-		discountedPrice: '₹84999.00',
-		discount: '₹10000.00',
-		monthlyPayment: '₹3750.00',
-	},
-	{
-		title: 'Nebula Tab FE (4G, 6GB RAM)',
-		color: 'Pink',
-		storage: '128 GB',
-		image: 'https://via.placeholder.com/150x200',
-		price: '₹29999.00',
-		discountedPrice: '₹35999.00',
-		discount: '₹6000.00',
-		monthlyPayment: '₹150x2000.00',
-	},
-];
-
 const Title = ({ title }) =>
-	<h3 className="text-lg font-semibold mt-4 h-16">{ title }</h3>;
+	<h3 className="text-lg font-semibold mt-4 h-5">{ title }</h3>;
 
 const Image = ({ src, alt }) =>
 	<div className="flex justify-center">
-		<img src={ src } alt={ alt } className="w-40 h-60 object-contain"/>
+		<img src={ src } alt={ alt } className="w-60 h-60 object-contain"/>
 	</div>;
 
 const Color = ({ color }) =>
@@ -114,10 +31,10 @@ const Storage = ({ storage }) =>
 		<p className="w-16 rounded-full border border-gray-600 p-1">{ storage }</p>
 	</div>;
 
-const Pricing = ({ monthlyPayment, price, discountedPrice, discount }) =>
+const Pricing = ({ price, discountedPrice, discount }) =>
 	<Fragment>
 		<p className="text-sm mt-2 font-semibold">
-			From { monthlyPayment } /mo for 24 mos at 15% Interest or { price }
+			₹ { price }
 		</p>
 		<p className="text-gray-400 line-through text-sm">{ discountedPrice }</p>
 		<p className="text-blue-500 text-sm mt-1">Save { discount }</p>
@@ -128,26 +45,27 @@ const BuyButton = () =>
 		Buy now
 	</button>;
 
-const ProductCard = ({ product }) =>
-	<div className="rounded-lg p-4 text-center">
-		<Title title={ product.title }/>
-		<Image src={ product.image } alt={ product.title }/>
-		<Color color={ product.color }/>
-		<ColorIndicator color={ product.color }/>
-		<Storage storage={ product.storage }/>
+const ProductCard = ({ product }) => {
+	const variant = product.variants[0];
+
+	return <div className="rounded-lg p-4 text-center">
+		<Title title={ product.name }/>
+		<Image src={ variant.imageUrls[0] } alt={ product.name }/>
+		<Color color={ variant.colorName }/>
+		<ColorIndicator color={ variant.color }/>
+		<Storage storage={ variant.storage }/>
 		<Pricing
-			monthlyPayment={ product.monthlyPayment }
-			price={ product.price }
-			discountedPrice={ product.discountedPrice }
-			discount={ product.discount }
+			price={ variant.price }
+			discountedPrice={ variant.discountedPrice }
+			discount={ variant.discount }
 		/>
 		<BuyButton/>
 	</div>;
+};
 
-const Products = () =>
+const Products = ({ products }) =>
 	<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-		{ products.map((product, index) =>
-			<ProductCard key={ index } product={ product }/>) }
+		{ products.map((product, index) => <ProductCard key={ index } product={ product }/>) }
 	</div>;
 
 export default Products;
--- frontend/src/components/Pages/Home/index.jsx
+++ frontend/src/components/Pages/Home/index.jsx
@@ -1,12 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import MetaData from '../../../MetaData';
 import Products from './Products';
+import { getProducts } from '../../../actions/productsActions';
 
-const Home = () =>
-	<main className=" mx-auto px-4 py-8">
+const Home = () => {
+	const dispatch = useDispatch();
+	const products = useSelector((state) => state.productState.products);
+
+	useEffect(() => {
+		dispatch(getProducts());
+	}, [dispatch]);
+
+	return <main className=" mx-auto px-4 py-8">
 		<MetaData title="Buy Best Products | Home "/>
 		<section className="mb-12">
-			<Products/>
+			<Products products={ products }/>
 		</section>
 	</main>;
+};
 
 export default Home;
