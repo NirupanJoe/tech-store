12c40a8e76802cb62839524a808133479c34bfea
Nirupan Joe <nirupanjoej@gmail.com>
2024-09-11 22:03:03 +0530

    Added error message display functionality.

---
+++ frontend/src/components/Pages/Error/index.jsx
@@ -0,0 +1,9 @@
+
+const Error = ({ error }) =>
+	<div className="text-blue-500 flex justify-center items-center
+   font-semibold text-center text-lg min-h-96 mb-4"
+	>
+		{ error }
+	</div>;
+
+export default Error;
--- frontend/src/components/Pages/Home/index.jsx
+++ frontend/src/components/Pages/Home/index.jsx
@@ -4,23 +4,28 @@ import MetaData from '../../../MetaData';
 import Products from './Products';
 import { getProducts } from '../../../actions/productsActions';
 import Loader from '../../Loader';
+import Error from '../Error';
 
 const Home = () => {
 	const dispatch = useDispatch();
-	const { products, loading } = useSelector((state) => state.productState);
+	const { products, loading, error } = useSelector((state) => state.productState);
 
 	useEffect(() => {
 		dispatch(getProducts());
 	}, [dispatch]);
 
-	return <main className=" mx-auto px-4 py-8">
-		<MetaData title="Buy Best Products | Home "/>
-		{ loading
-			? <Loader/>
-			: <section className="mb-12">
-				<Products { ...{ products } }/>
-			</section> }
-	</main>;
+	return (
+		<main className="mx-auto px-4 py-8">
+			<MetaData title="Buy Best Products | Home"/>
+			{ loading
+				? <Loader/>
+				: error
+					? <Error { ...{ error } }/>
+					: <section className="mb-12">
+						<Products { ...{ products } }/>
+					</section> }
+		</main>
+	);
 };
 
 export default Home;
