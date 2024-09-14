de595340f5345ea8cd027940fbe2417e0a13585f
Nirupan Joe <nirupanjoej@gmail.com>
2024-09-14 15:12:34 +0530

    Added the primary color in tailwind.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,24 @@ export default {
 		'./index.html',
 		'./src/**/*.{js,ts,jsx,tsx}',
 	],
-	theme: { extend: {}},
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					50: '#E3F2FF',
+					100: '#BAE0FF',
+					200: '#7FC6FF',
+					300: '#4AAFFF',
+					400: '#2198FF',
+					500: '#2189FF',
+					600: '#1B70D6',
+					700: '#1558A8',
+					800: '#0F407A',
+					900: '#09295C',
+					950: '#04122D',
+				},
+			},
+		},
+	},
 	plugins: [],
 };
