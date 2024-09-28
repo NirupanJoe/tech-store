1ec748f6ebe1ff8e065f6b38cda87e85886c1f7d
Nirupan Joe <nirupanjoej@gmail.com>
2024-09-28 22:26:01 +0530

    Add "select-none" class to Carousel container div.

--- frontend/src/components/Carousel/index.jsx
+++ frontend/src/components/Carousel/index.jsx
@@ -63,7 +63,7 @@ const Carousel = ({ images }) => {
 	const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
 	return (
-		<div className="max-w-3xl mx-auto">
+		<div className="max-w-3xl mx-auto select-none">
 			<MainSwiper images={ images } thumbsSwiper={ thumbsSwiper }/>
 			<ThumbnailSwiper images={ images } setThumbsSwiper={ setThumbsSwiper }/>
 		</div>
