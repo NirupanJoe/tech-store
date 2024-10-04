018f2d6810ee025cd0cf790aa1a5f1ca80d7fa42
Nirupan Joe <nirupanjoej@gmail.com>
2024-10-04 21:05:40 +0530

    Created the productSearch.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './components/Pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import ProductDetails from './components/Pages/ProductDetails';
+import ProductSearch from './components/Pages/productSearch';
 
 const App = () =>
 	<div>
@@ -11,6 +12,7 @@ const App = () =>
 		<Routes>
 			<Route path="/" element={ <Home/> }/>
 			<Route path="/product/:id" element={ <ProductDetails/> }/>
+			<Route path="/search/:keyword" element={ <ProductSearch/> }/>
 		</Routes>
 		<Footer/>
 	</div>;
---
+++ frontend/src/components/Pages/productSearch/index.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import MetaData from '../../../MetaData';
+import Products from '../Home/Products';
+import { getProducts } from '../../../actions/productsActions';
+import Loader from '../../Loader';
+import Error from '../Error';
+import { useParams } from 'react-router-dom';
+
+const RenderContent = ({ products, loading, error, productsProps }) =>
+	(loading
+		? <Loader/>
+		: error
+			? <Error { ...{ error } }/>
+			: products.length > 0 && <Products { ...productsProps }/>);
+
+const ProductSearch = () => {
+	const dispatch = useDispatch();
+	const productsState = useSelector((state) => state.productsState);
+	const [currentPage, setCurrentPage] = useState(1);
+	const productsProps = { ...productsState, currentPage, setCurrentPage };
+	const { keyword } = useParams();
+
+	useEffect(() => {
+		dispatch(getProducts({ currentPage, keyword }));
+	}, [dispatch, currentPage, keyword]);
+
+	return (
+		<main className="mx-auto px-4 py-8">
+			<MetaData title="ProductSearch"/>
+			<RenderContent { ...{ ...productsState, productsProps } }/>
+		</main>
+	);
+};
+
+export default ProductSearch;
