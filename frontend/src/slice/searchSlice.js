84e7242e9eeeb01ee4a842f268f23be210c2494f
Nirupan Joe <nirupanjoej@gmail.com>
2024-10-05 14:51:17 +0530

    Added local storage functionality to searchSlice to persist search history.

--- frontend/src/slice/searchSlice.js
+++ frontend/src/slice/searchSlice.js
@@ -1,17 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
 import config from '../actions/config';
 
+const saveToLocalStorage = (key, value) => {
+	try {
+		localStorage.setItem(key, JSON.stringify(value));
+	}
+	catch (error) {
+		console.error('Error saving to local storage', error);
+	}
+};
+
+const loadFromLocalStorage = (key) => {
+	try {
+		const data = localStorage.getItem(key);
+
+		return data ? JSON.parse(data) : [];
+	}
+	catch (error) {
+		console.error('Error loading from local storage', error);
+		return [];
+	}
+};
+
 const searchSlice = createSlice({
 	name: 'search',
-	initialState: { searchHistory: [] },
+	initialState: { searchHistory: loadFromLocalStorage('searchHistory') },
 	reducers: {
 		addSearchHistory (state, action) {
 			const newHistory = [action.payload, ...state.searchHistory];
 
 			state.searchHistory = Array.from(new Set(newHistory)).slice(0, config.maxSearchHistory);
+
+			saveToLocalStorage('searchHistory', state.searchHistory);
 		},
-		clearHistory () {
-			return { searchHistory: [] };
+		clearHistory (state) {
+			state.searchHistory = [];
+
+			saveToLocalStorage('searchHistory', []);
 		},
 	},
 });
