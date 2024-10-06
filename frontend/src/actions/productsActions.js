1f3ef5478bac1146550c9c095df2b0b8d0f6472d
Nirupan Joe <nirupanjoej@gmail.com>
2024-10-06 17:46:57 +0530

    Refactor getProducts action to generate query string using new function.

--- frontend/src/actions/productsActions.js
+++ frontend/src/actions/productsActions.js
@@ -4,16 +4,24 @@ import config from './config';
 
 const options = { headers: { 'Cache-Control': 'no-cache' }};
 
-export const getProducts = ({ currentPage, keyword }) => async (dispatch) => {
+const generateQueryString = ({ currentPage, category, keyword, productsPerPage }) => {
+	const queryParams = {
+		page: currentPage,
+		limit: productsPerPage,
+		...keyword && { keyword },
+		...category && { category },
+	};
+
+	return new URLSearchParams(queryParams).toString();
+};
+
+export const getProducts = ({ currentPage, category, keyword }) => async (dispatch) => {
 	const { productsPerPage } = config;
+	const queryString = generateQueryString({ currentPage, category, keyword, productsPerPage });
+	const link = `/api/products?${ queryString }`;
 
 	try {
 		dispatch(productsRequest());
-		let link = `/api/products?page=${ currentPage }&limit=${ productsPerPage }`;
-
-		if(keyword)
-			link += `&keyword=${ keyword }`;
-
 		const response = await axios.get(link, options);
 
 		dispatch(productsSuccess(response.data));
