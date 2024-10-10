a4008efe8e1a99cf389df2be454762338028f0d0
Nirupan Joe <nirupanjoej@gmail.com>
2024-10-10 20:27:04 +0530

    Update linkClass styling in ProductPagination component.

--- frontend/src/components/Products/ProductPagination.jsx
+++ frontend/src/components/Products/ProductPagination.jsx
@@ -10,8 +10,8 @@ const ProductPagination = ({ currentPage, setCurrentPage, productsCount }) =>
 				totalItemsCount={ productsCount }
 				onChange={ (page) => setCurrentPage(page) }
 				itemClass="inline-block mx-1"
-				linkClass="px-4 py-2 border border-primary-500 rounded-full text-gray-600
-					hover:bg-primary-600 hover:text-white"
+				linkClass="px-2 py-1 sm:px-4 sm:py-2 border border-primary-500 rounded-full
+					text-gray-600 hover:bg-primary-600 hover:text-white text-sm sm:text-base"
 				activeLinkClass="bg-primary-600 text-white"
 				disabledClass="opacity-50 cursor-not-allowed"
 			/>
