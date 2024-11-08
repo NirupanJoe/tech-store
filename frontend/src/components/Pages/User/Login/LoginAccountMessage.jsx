49c3d022aeae3d067f1997ccefcaacb9acd4bd59
Nirupan Joe <nirupanjoej@gmail.com>
2024-11-08 13:22:46 +0530

    Swap login and register account messages.

--- frontend/src/components/Pages/User/Login/LoginAccountMessage.jsx
+++ frontend/src/components/Pages/User/Login/LoginAccountMessage.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 const LoginAccountMessage = () =>
 	<div className="mt-5 sm:mx-auto sm:w-full sm:max-w-md">
 		<div className="bg-white py-4 px-4 shadow text-center sm:rounded-lg sm:px-6">
-			Already have an account? <Link to="/register" className="text-primary-500">
-				Sign in →
+			New to TechStore?{ ' ' }
+			<Link to="/register" className="text-primary-500">
+				Create an account
 			</Link>
 		</div>
 	</div>;
--- frontend/src/components/Pages/User/Register/RegisterAccountMessage.jsx
+++ frontend/src/components/Pages/User/Register/RegisterAccountMessage.jsx
@@ -3,9 +3,8 @@ import { Link } from 'react-router-dom';
 const RegisterAccountMessage = () =>
 	<div className="mt-5 sm:mx-auto sm:w-full sm:max-w-md">
 		<div className="bg-white py-4 px-4 shadow text-center sm:rounded-lg sm:px-6">
-			New to TechStore?{ ' ' }
-			<Link to="/login" className="text-primary-500">
-				Create an account
+			Already have an account? <Link to="/login" className="text-primary-500">
+				Sign in →
 			</Link>
 		</div>
 	</div>;
