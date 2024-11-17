aa71164457c3d28fd5ac7b12c9af49ea0e141d31
Nirupan Joe <nirupanjoej@gmail.com>
2024-11-17 15:28:37 +0530

    Fixed the ProtectedRoute.

--- frontend/src/components/Route/ProtectedRoute.jsx
+++ frontend/src/components/Route/ProtectedRoute.jsx
@@ -1,13 +1,18 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import Loader from '../Loader';
 
 const ProtectedRoute = ({ children }) => {
-	const { isAuthenticated } = useSelector((state) => state.authState);
+	const { isAuthenticated, loading } = useSelector((state) => state.authState);
 
-	if(!isAuthenticated)
+	if(!isAuthenticated && !loading)
 		return <Navigate to="/login"/>;
 
-	return children;
+	if(isAuthenticated)
+		return children;
+
+	if(loading)
+		return <Loader/>;
 };
 
 export default ProtectedRoute;
