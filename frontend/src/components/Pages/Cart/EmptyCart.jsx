9b6880374805fd54f7bee1707c53afaf5617a8e2
Nirupan Joe <nirupanjoej@gmail.com>
2024-12-07 14:26:41 +0530

    Fixed the bug.

--- frontend/src/components/Pages/Cart/EmptyCart.jsx
+++ frontend/src/components/Pages/Cart/EmptyCart.jsx
@@ -1,8 +1,7 @@
 
-const EmptyCart = () => {
+const EmptyCart = () =>
 	<div className="text-center py-10">
 		<p className="text-xl text-gray-500">Your cart is empty</p>
 	</div>;
-};
 
 export default EmptyCart;
