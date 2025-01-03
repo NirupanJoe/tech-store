967139be272daba9374e00ed60d9c0e10f20d887
Nirupan Joe <nirupanjoej@gmail.com>
2025-01-03 16:12:25 +0530

    Convert payment amount from rupees to cents for Stripe integration.

--- backend/controllers/payment.js
+++ backend/controllers/payment.js
@@ -3,9 +3,11 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const asyncHandler = require('../utils/asyncHandler');
 const Status = require('../utils/statusEnum');
 
+const centsPerRupee = 100;
+
 exports.processPayment = asyncHandler(async (req, res) => {
 	const myPayment = await stripe.paymentIntents.create({
-		amount: req.body.amount,
+		amount: req.body.amount * centsPerRupee,
 		currency: 'inr',
 		metadata: {
 			company: 'Ecommerce',
