c9cdd465ce70d17476413f13412cb717fc42db66
Nirupan Joe <nirupanjoej@gmail.com>
2025-01-03 16:12:46 +0530

    Enhance Button component to accept additional className and props for better customization.

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,10 @@
 
-const Button = ({ isDisabled, label }) =>
+const Button = ({ isDisabled, label, className, ...props }) =>
 	<button
 		disabled={ isDisabled }
 		className={ `md:w-80 w-full h-10 py-2 my-6 rounded-full text-white
-				${ isDisabled ? 'bg-neutral-300 text-neutral-400' : 'bg-primary-500' }` }
+				${ isDisabled ? 'bg-neutral-300 text-neutral-400' : 'bg-primary-500' } ${ className }` }
+		{ ...props }
 	>
 		{ label }
 	</button>;
