ed320af68158847daf92489836c4f629a5bf022d
Nirupan Joe <nirupanjoej@gmail.com>
2025-01-04 14:05:12 +0530

    Update payment processing to round amount and include shipping details.

--- backend/controllers/payment.js
+++ backend/controllers/payment.js
@@ -3,12 +3,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const asyncHandler = require('../utils/asyncHandler');
 const Status = require('../utils/statusEnum');
 
-const centsPerRupee = 100;
-
 exports.processPayment = asyncHandler(async (req, res) => {
 	const myPayment = await stripe.paymentIntents.create({
-		amount: req.body.amount * centsPerRupee,
+		amount: Math.round(req.body.amount),
 		currency: 'inr',
+		description: 'Test payment',
+		shipping: req.body.shipping,
 		metadata: {
 			company: 'Ecommerce',
 		},
