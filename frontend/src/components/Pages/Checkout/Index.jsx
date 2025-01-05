bd118da3f864238c9f72132f20b6d56af1704746
Nirupan Joe <nirupanjoej@gmail.com>
2025-01-05 16:50:27 +0530

    Redirect to cart if no items are present in the Checkout component.

--- frontend/src/components/Pages/Checkout/Index.jsx
+++ frontend/src/components/Pages/Checkout/Index.jsx
@@ -1,12 +1,21 @@
+import { useSelector } from 'react-redux';
 import ContactInfo from './ContactInfo';
 import ViewOrder from './ViewOrder';
+import { useNavigate } from 'react-router-dom';
 
-const Checkout = () =>
-	<div className="container mx-auto px-1 py-8">
+const Checkout = () => {
+	const { items } = useSelector((state) => state.cartState);
+	const navigate = useNavigate();
+
+	if(items.length === 0)
+		navigate('/cart');
+
+	return <div className="container mx-auto px-1 py-8">
 		<div className="grid grid-cols-1 md:grid-cols-2 md:gap-8">
 			<ContactInfo/>
 			<ViewOrder/>
 		</div>
 	</div>;
+};
 
 export default Checkout;
