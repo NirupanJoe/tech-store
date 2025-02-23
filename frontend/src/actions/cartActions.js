454e96f19c8aaa279d763fc495ae61782dfbed4c
Nirupan Joe <84219089+NirupanJoe@users.noreply.github.com>
2025-02-23 09:59:55 +0000

    Add additional fields to order schema and refactor cart item creation logic.

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,20 +1,25 @@
 import { addCartItemFail, addCartItemRequest, addCartItemSuccess } from '../slice/cartSlice';
 import { v4 as uuid } from 'uuid';
 
+const createCartItemData = (product, selectedVariant) => ({
+	id: uuid(),
+	productId: product._id,
+	variantId: selectedVariant._id,
+	name: product.name,
+	price: selectedVariant.discountedPrice,
+	image: selectedVariant.imageUrls[0],
+	color: selectedVariant.color,
+	ram: selectedVariant.ram,
+	storage: selectedVariant.storage,
+	colorName: selectedVariant.colorName,
+	data: { ...product, ...selectedVariant },
+	qty: 1,
+});
+
 export const addCartItem = ({ product, selectedVariant }) => (dispatch) => {
 	try {
 		dispatch(addCartItemRequest());
-		const data = {
-			id: uuid(),
-			productId: product._id,
-			variantId: selectedVariant._id,
-			name: product.name,
-			price: selectedVariant.discountedPrice,
-			image: selectedVariant.imageUrls[0],
-			color: selectedVariant.colorName,
-			data: { ...product, ...selectedVariant },
-			qty: 1,
-		};
+		const data = createCartItemData(product, selectedVariant);
 
 		dispatch(addCartItemSuccess(data));
 	}
